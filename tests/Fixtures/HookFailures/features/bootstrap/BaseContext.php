2b279b0f2e26d4687772aa9135a0e9581a2261be
Carlos Granados <barel.barelon@gmail.com>
2025-10-30 03:06:08 +0100

    Convert Failed Hooks JSON test to fixtures tests

--- tests/Fixtures/HookFailures/features/bootstrap/BaseContext.php
+++ tests/Fixtures/HookFailures/features/bootstrap/BaseContext.php
@@ -1,11 +1,10 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Exception;
 
 abstract class BaseContext implements Context
 {
-    protected static bool $thrown = false;
+    private static bool $thrown = false;
 
     protected static function throwFailure(string $message): void
     {
