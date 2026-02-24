cf3a48bf3bc5e33587e55aa9c0b9c987adb9fbbf
acoulton <andrew@ingenerator.com>
2026-02-24 16:06:50 +0000

    feat: drop config conversion support for GherkinOptions
    This object was added in 3.x after the PR that removed support for YAML
    config and the config conversion tools in 4.x

--- src/Behat/Config/GherkinOptions.php
+++ src/Behat/Config/GherkinOptions.php
@@ -2,25 +2,15 @@
 
 namespace Behat\Config;
 
-use Behat\Config\Converter\ConfigConverterTools;
 use Behat\Config\Filter\FilterInterface;
-use Behat\Config\Filter\NameFilter;
-use Behat\Config\Filter\NarrativeFilter;
-use Behat\Config\Filter\RoleFilter;
-use Behat\Config\Filter\TagFilter;
 use Behat\Gherkin\GherkinCompatibilityMode;
 use Behat\Testwork\ServiceContainer\Exception\ConfigurationLoadingException;
-use PhpParser\Node\Expr;
 
-final class GherkinOptions implements ConfigConverterInterface
+final class GherkinOptions
 {
     private const CACHE_SETTING = 'cache';
     private const COMPATIBILITY_SETTING = 'compatibility';
     private const FILTERS_SETTING = 'filters';
-
-    private const CACHE_FUNCTION = 'withCacheDir';
-    private const COMPATIBILITY_FUNCTION = 'withCompatibilityMode';
-    private const FILTER_FUNCTION = 'withFilter';
 
     public function __construct(
         private array $settings = [],
@@ -66,78 +56,5 @@ final class GherkinOptions implements ConfigConverterInterface
         $this->settings[self::FILTERS_SETTING][$filter->name()] = $filter->value();
 
         return $this;
-    }
-
-    /**
-     * @internal
-     */
-    public function toPhpExpr(): Expr
-    {
-        $optionsObject = ConfigConverterTools::createObject(self::class);
-        $expr = $optionsObject;
-
-        $this->addCacheToExpr($expr);
-        $this->addCompatibilityModeToExpr($expr);
-        $this->addFiltersToExpr($expr);
-
-        $arguments = count($this->settings) === 0 ? [] : [$this->settings];
-        ConfigConverterTools::addArgumentsToConstructor($arguments, $optionsObject);
-
-        return $expr;
-    }
-
-    private function addCacheToExpr(Expr &$expr): void
-    {
-        if (isset($this->settings[self::CACHE_SETTING])) {
-            $expr = ConfigConverterTools::addMethodCall(
-                self::class,
-                self::CACHE_FUNCTION,
-                [$this->settings[self::CACHE_SETTING]],
-                $expr
-            );
-            unset($this->settings[self::CACHE_SETTING]);
-        }
-    }
-
-    private function addCompatibilityModeToExpr(Expr &$expr): void
-    {
-        if (isset($this->settings[self::COMPATIBILITY_SETTING])) {
-            $expr = ConfigConverterTools::addMethodCall(
-                self::class,
-                self::COMPATIBILITY_FUNCTION,
-                [GherkinCompatibilityMode::from($this->settings[self::COMPATIBILITY_SETTING])],
-                $expr
-            );
-            unset($this->settings[self::COMPATIBILITY_SETTING]);
-        }
-    }
-
-    private function addFiltersToExpr(Expr &$expr): void
-    {
-        if (!isset($this->settings[self::FILTERS_SETTING])) {
-            return;
-        }
-
-        foreach ($this->settings[self::FILTERS_SETTING] as $name => $filterValue) {
-            $filter = match ($name) {
-                NameFilter::NAME => new NameFilter($filterValue),
-                NarrativeFilter::NAME => new NarrativeFilter($filterValue),
-                RoleFilter::NAME => new RoleFilter($filterValue),
-                TagFilter::NAME => new TagFilter($filterValue),
-                default => null,
-            };
-            if ($filter !== null) {
-                $expr = ConfigConverterTools::addMethodCall(
-                    self::class,
-                    self::FILTER_FUNCTION,
-                    [$filter->toPhpExpr()],
-                    $expr
-                );
-                unset($this->settings[self::FILTERS_SETTING][$name]);
-            }
-        }
-        if ($this->settings[self::FILTERS_SETTING] === []) {
-            unset($this->settings[self::FILTERS_SETTING]);
-        }
     }
 }
